937efdcbb32a9994ccfdfeac9fd27d983016a054
Nikolay <46003534+riednyko@users.noreply.github.com>
2020-01-21 20:08:55 +0300

    Update build-snap.sh

--- snap/build-snap.sh
+++ snap/build-snap.sh
@@ -5,6 +5,6 @@ cd ./snapcraft/
 sudo snap install lxd
 snapcraft clean dbeaver-ce --step pull
 snapcraft cleanbuild
-snapcraft login
+snapcraft login --with exported
 OUTPUT="$(ls -Art | tail -n 1)"
 snapcraft push $OUTPUT --release=stable
