366271487a7bc86d19b3bd64ee3f5b17b6c7a9ff
Nikolay <46003534+riednyko@users.noreply.github.com>
2020-01-22 15:34:34 +0300

    Update build-snap.sh

--- snap/build-snap.sh
+++ snap/build-snap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-lxc exec snapcraft -- apt install squashfuse
 cp snapcraft.yaml ./snapcraft/snap/
 cd ./snapcraft/
 snapcraft clean dbeaverapp --step pull
+pwd
 snapcraft cleanbuild
 pwd
 snapcraft login --with ../snapcraft.login
