05c5dd717f172e30d8a126ec2566b0dd58283e72
Nikolay <46003534+riednyko@users.noreply.github.com>
2020-01-22 12:36:22 +0300

    Update build-snap.sh

--- snap/build-snap.sh
+++ snap/build-snap.sh
@@ -5,6 +5,6 @@ cd ./snapcraft/
 snapcraft clean dbeaver-ce --step pull
 snapcraft cleanbuild
 pwd
-snapcraft login --with snapcraft.login
+snapcraft login --with ../snapcraft.login
 OUTPUT="$(ls -Art | tail -n 1)"
 snapcraft push $OUTPUT --release=stable
